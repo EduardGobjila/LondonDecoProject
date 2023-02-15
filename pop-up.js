d161867a36372250f12b658adf716f345b311c3e
Eduard Gobjila <edikgobjila01@gmail.com>
2023-02-15 15:42:28 +0000

    added a function to display the button on scroll from 210

--- pop-up.js
+++ pop-up.js
@@ -2,10 +2,19 @@
 const button = document.getElementById('close')
 const container = document.getElementsByClassName('pop-up-container')[0]
 
-
-console.log(container)
+counter = 0
 button.addEventListener('click', () => {
   container.style = 'display: none;'
+  counter += 1
 })
 
-// console.log('checking in production')
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > '210') {
+    if (counter == 0)  {
+      container.style = 'display: block;'
+    } else {
+      container.style = 'display: none;'
+    }
+  }
+})
